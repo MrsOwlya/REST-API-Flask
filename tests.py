9094e07061ef2e85e4ed52c94b5fb67b36098bfd
MrsOwlya <olja-holmes@mail.ru>
2021-06-20 14:51:12 +0300

    Clean pytest

--- tests.py
+++ tests.py
@@ -6,8 +6,8 @@ from app import app, db, Person
 def test_client():
     app.config['TESTING'] = True
     with app.test_client() as testing_client:
-            with app.app_context():
-                yield testing_client
+        with app.app_context():
+            yield testing_client
 
 
 @pytest.fixture(scope='module')
@@ -39,55 +39,53 @@ def init_database(test_client):
     db.drop_all()
 
 
-class TestCase:
+def test_get_all_persons(test_client, init_database):
+    res = test_client.get('/persons')
+    assert res.status_code == 200
+    assert len(res.get_json()) == len(Person.query.all())
 
-    def test_get_all_persons(self, test_client, init_database):
-        res = test_client.get('/persons')
-        assert res.status_code == 200
-        assert len(res.get_json()) == len(Person.query.all())
+def test_get_person_by_id(test_client, init_database):
+    res = test_client.get('/persons/1')
+    assert res.status_code == 200
+    assert Person.query.get(1).firstname == 'TestFirstName'
 
-    def test_get_person_by_id(self, test_client, init_database):
-        res = test_client.get('/persons/1')
-        assert res.status_code == 200
-        assert Person.query.get(1).firstname == 'TestFirstName'
+def test_get_person_not_exist(test_client, init_database):
+    res = test_client.get('/persons/3000')
+    assert res.status_code == 404
 
-    def test_get_person_not_exist(self, test_client, init_database):
-        res = test_client.get('/persons/3000')
-        assert res.status_code == 404
+def test_post_new_person(test_client, init_database):
+    data = {
+        "firstname": "UnitName",
+        "surname": "UnitSur",
+        "birth": "1998-04-17",
+        "job": "UnitJob",
+        "address": "UnitAddress"
+    }
+    res = test_client.post('/persons', json=data)
+    assert res.status_code == 201
+    assert Person.query.get(3).firstname == data['firstname']
 
-    def test_post_new_person(self, test_client, init_database):
-        data = {
-            "firstname": "UnitName",
-            "surname": "UnitSur",
-            "birth": "1998-04-17",
-            "job": "UnitJob",
-            "address": "UnitAddress"
-        }
-        res = test_client.post('/persons', json=data)
-        assert res.status_code == 201
-        assert Person.query.get(3).firstname == data['firstname']
+def test_post_new_invalid_person(test_client, init_database):
+    data = {
+        "firstname": "UnitName",
+        "birth": "1998-04-17",
+        "job": "UnitJob",
+        "address": "UnitAddress"
+    }
+    res = test_client.post('/persons', json=data)
+    assert res.status_code == 422
 
-    def test_post_new_invalid_person(self, test_client, init_database):
-        data = {
-            "firstname": "UnitName",
-            "birth": "1998-04-17",
-            "job": "UnitJob",
-            "address": "UnitAddress"
-        }
-        res = test_client.post('/persons', json=data)
-        assert res.status_code == 422
+def test_patch_person(test_client, init_database):
+    data = {
+        "firstname": "UnitName2",
+        "job": "UnitJob2"
+    }
+    res = test_client.patch('/persons/1', json=data)
+    assert res.status_code == 200
+    assert Person.query.get(1).firstname == 'UnitName2'
+    assert Person.query.get(1).job == 'UnitJob2'
 
-    def test_patch_person(self, test_client, init_database):
-        data = {
-            "firstname": "UnitName2",
-            "job": "UnitJob2"
-        }
-        res = test_client.patch('/persons/1', json=data)
-        assert res.status_code == 200
-        assert Person.query.get(1).firstname == 'UnitName2'
-        assert Person.query.get(1).job == 'UnitJob2'
-
-    def test_delete_person(self, test_client, init_database):
-        res = test_client.delete('/persons/2')
-        assert res.status_code == 204
-        assert Person.query.get(2) is None
+def test_delete_person(test_client, init_database):
+    res = test_client.delete('/persons/2')
+    assert res.status_code == 204
+    assert Person.query.get(2) is None
