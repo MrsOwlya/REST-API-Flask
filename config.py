3af7d5b5fc7cc7b27941aaae5eebdb71eb823e45
MrsOwlya <olja-holmes@mail.ru>
2021-06-21 00:02:24 +0300

    Update config

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ from apispec.ext.marshmallow import MarshmallowPlugin
 
 
 class Config:
-    if os.environ.get('GITHUB_JOB') == 'my_deploy':
-        database_path = "postgres://lhusbwbrroozdo:e304101adc4967768d13ea81ac4f5cd4c2a75806f6249dbb4af6b1014991d391@ec2-50-17-255-120.compute-1.amazonaws.com:5432/d2tcus1l0luhaf"
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', database_path)
-    else:
+    if os.environ.get('DATABASE_URL') is None:
         SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://owlya:sveya@localhost:5432/test_db'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '025b376adf584b72888bffe69f90524e'
     APISPEC_SPEC = APISpec(
